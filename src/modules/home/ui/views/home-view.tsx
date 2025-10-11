b94a1b40e64e495f9b27906b6e796c1810613a48
AsemHagiHasan <asemhagihasan12345@gmail.com>
2025-10-11 20:32:43 +0300

    09: agents setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,16 +1,7 @@
 "use client";
-import { authClient } from "@/lib/auth-client";
 
 const HomeView = () => {
-  const { data: session, isPending } = authClient.useSession();
-
-  if (isPending) return <p>Loading...</p>;
-
-  return (
-    <div className="flex flex-col gap-y-4 p-4">
-      <p className="">Logged in as {session?.user?.email}</p>
-    </div>
-  );
+  return <div>home view</div>;
 };
 
 export default HomeView;
